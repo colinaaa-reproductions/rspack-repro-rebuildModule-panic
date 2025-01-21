8b5a7fec031754047c7217da0691718fe8a763a0
Qingyu Wang <wangqingyu.c0l1n@tiktok.com>
2025-01-21 10:29:11 +0800

    Update config

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,6 +1,8 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+
+import { rspack } from '@rspack/core'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
@@ -8,6 +10,8 @@ const isRunningRspack = !!process.env.RSPACK;
 if (!isRunningRspack && !isRunningWebpack) {
   throw new Error("Unknown bundler");
 }
+
+const CssExtractPlugin = isRunningRspack ? rspack.CssExtractRspackPlugin : MiniCssExtractPlugin;
 
 /**
  * @type {import('webpack').Configuration | import('@rspack/cli').Configuration}
@@ -18,16 +22,48 @@ const config = {
   entry: {
     main: "./src/index",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+           CssExtractPlugin.loader,
+          'css-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CssExtractPlugin(),
+    /**
+      * @param {import('@rspack/core').Compiler} compiler 
+      */
+    (compiler) => {
+      let initial = true;
+      compiler.hooks.thisCompilation.tap('test', (compilation) => {
+        compilation.hooks.finishModules.tap('test', (modules) => {
+          if (!initial) {
+            return
+          }
+          initial = false;
+
+          for (const module of modules) {
+            if (module.resource?.includes('.css')) {
+              compilation.rebuildModule(module, err => {
+                console.log('rebuildModule failed', err)
+              })
+            }
+          }
+        })
+      })
+    }
+  ],
   output: {
     clean: true,
     path: isRunningWebpack
       ? path.resolve(__dirname, "webpack-dist")
       : path.resolve(__dirname, "rspack-dist"),
     filename: "[name].js",
-  },
-  experiments: {
-    css: true,
   },
 };
 
